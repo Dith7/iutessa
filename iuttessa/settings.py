314d9db84a22cf3ab1dbb4985e44b7facd4c9ab1
davyemane <davyemane2@gmail.com>
2025-10-12 20:53:20 +0100

    test

--- iuttessa/settings.py
+++ iuttessa/settings.py
@@ -193,7 +193,8 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/app/media'  # Chemin absolu en Docker
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Taille maximale des fichiers
 FILE_UPLOAD_MAX_MEMORY_SIZE = 50 * 1024 * 1024  # 50MB
